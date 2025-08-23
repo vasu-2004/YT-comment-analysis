38032a9eb1455687d9c80df6ac2b7017da5a6aac
vasu-2004 <santoshsidhuom@gmail.com>
2025-08-23 17:52:32 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a small chrome plugin to do realtime yt comment setiment analysis',
+    author='Vasu',
+    license='',
+)
